b74645928d9b49763c0dbffaec46f6685f576b22
afridisadam1-alt <afridi.sadam1@gmail.com>
2025-10-19 21:17:25 +0100

    server changes

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var http = require("http");
 const port = 8000; 
 
 http.createServer(function(req, res) { 
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello Worlddddddd!");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<!DOCTYPE html><html><body><h1>Hello World! <h1>Want to check changes on Server side</h1></h1><p>This is a paragraph.</p></body></html>");
+
     }).listen(port, function() { 
         console.log(`Node server is running on port ${port}...`); 
 }); 
